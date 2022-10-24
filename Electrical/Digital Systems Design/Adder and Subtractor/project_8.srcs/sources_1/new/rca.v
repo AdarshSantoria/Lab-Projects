d581b96c33991cb04223b3d6a4b02f02a2e950a1
AdarshSantoria <adarshsantoria@gmail.com>
2022-10-24 16:14:20 +0530

    commit

---
+++ Electrical/Digital Systems Design/Adder and Subtractor/project_8.srcs/sources_1/new/rca.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.10.2022 16:00:00
+// Design Name: 
+// Module Name: rca
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module rca(
+
+    );
+endmodule
